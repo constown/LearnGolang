1db40fe4ba12a4f61de5aba5f68ba65ec0e29751
ConstOwn <constown@foxmail.com>
2021-06-05 11:35:41 +0800

    feat: 🎉整型

--- 整型/main.go
+++ 整型/main.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
+// 在使用 int 和 uint 类型时，不能假定他是32位或者64位的整型，而是要考虑不同平台的差异
+// uintptr 用于存放一个指针
+
 func main() {
+	//	go语言中无法直接定义二进制数
+
 	// 十进制
 	var i1 = 101
 	fmt.Printf("%d\n", i1) // 十进制
@@ -11,11 +16,19 @@ func main() {
 	fmt.Printf("%x\n", i1) // 十六机制
 
 	// 八进制
-	var i2 = 007
+	i2 := 077
 	fmt.Printf("%d\n", i2)
 
 	// 十六进制
 	var i3 = 0x1234567
+	fmt.Printf("%d\n", i3)
 
-	fmt.Printf("%x\n", i3)
+	// 查看变量的类型
+	fmt.Printf("%T\n", i3)
+
+	//	声明 int8 类型的变量
+
+	i4 := int8(9) // 明确指定int8类型，否则就是默认为int类型
+	fmt.Printf("%T\n", i4)
+
 }
