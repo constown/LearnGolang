913633cfc4e7ca098f989ea14caf7fcfe0d52956
ConstOwn <constown@foxmail.com>
2021-06-07 02:15:00 +0800

    feat: 🎉切片

--- 切片/main.go
+++ 切片/main.go
@@ -11,18 +11,19 @@ func main() {
 	s1 = []int{1, 2, 3}
 	s2 = []string{"const", "let", "var", "key"}
 	fmt.Println(s1, s2)
+	// len() 长度  cap() 容量
 	fmt.Printf("len(s1): %d cap(s1):%d\n", len(s1), cap(s1))
 	fmt.Printf("len(s2): %d cap(s2):%d\n", len(s2), cap(s2))
 	// 由数组得到切片
 	a1 := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	s3 := a1[0:4]   // 基于数组去切割，左包含右不包含（左闭右开）
-	s4 := a1[:4]    // 从零切到4 => [0:4]
-	s5 := a1[2:]    // 从2切到末尾 => [2:len(ai)]
-	s6 := a1[:]     // 从头到尾 => [0: len(a1)]
-	fmt.Println(s3) // => [1 2 3 4]
-	fmt.Println(s4) // => [1 2 3 4]
-	fmt.Println(s5) // => [3 4 5 6 7 8 9]
-	fmt.Println(s6) // => [1 2 3 4 5 6 7 8 9]
+	s3 := a1[0:4]          // 基于数组去切割，左包含右不包含（左闭右开）
+	s4 := a1[:4]           // 从零切到4 => [0:4]
+	s5 := a1[2:]           // 从2切到末尾 => [2:len(ai)]
+	s6 := a1[:]            // 从头到尾 => [0: len(a1)]
+	fmt.Println("s3:", s3) // => [1 2 3 4]
+	fmt.Println("s4:", s4) // => [1 2 3 4]
+	fmt.Println("s5:", s5) // => [3 4 5 6 7 8 9]
+	fmt.Println("s6:", s6) // => [1 2 3 4 5 6 7 8 9]
 	// 切片的容量是指底层数组的容量（底层数组从切片的第一个元素到最后的元素的数量）
 	// 切片的长度就是元素的个数
 	fmt.Printf("len(s4):%d cap(s4):%d\n", len(s4), cap(s4)) // => len(s4):4 cap(s4):9
@@ -31,4 +32,11 @@ func main() {
 	s7 := s5[3:]
 	fmt.Println(s7)                                         // => [6 7 8 9]
 	fmt.Printf("len(s7):%d cap(s7):%d\n", len(s7), cap(s7)) // => len(s7):4 cap(s7):4
+	//切片是引用类型， 底层都指向了一个数组
+	a1[3] = 400
+	a1[7] = 800
+	fmt.Println("s3:", s3) // => [1 2 3 400]
+	fmt.Println("s4:", s4) // => [1 2 3 400]
+	fmt.Println("s5:", s5) // => [3 4 5 6 7 800 9]
+	fmt.Println("s6:", s6) // => [1 2 3 4 5 6 7 800 9]
 }
