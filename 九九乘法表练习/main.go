fa886488cd08ee37c1585a242314fa2d806c86f7
ConstOwn <constown@foxmail.com>
2021-06-06 23:46:05 +0800

    feat: 🎉九九乘法表练习

--- 九九乘法表练习/main.go
+++ 九九乘法表练习/main.go
@@ -1,16 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	for i := 1; i < 10; i++ {
-		for n := 1; n < 10; n++ {
-			if n > i {
-				fmt.Println()
-				break
-			} else {
-				fmt.Printf("%d*%d=%d  ", n, i, i*n)
-			}
+	fmt.Println("1、==========")
+	for i := 1; i <= 9; i++ {
+		for n := 1; n <= i; n++ {
+			fmt.Printf("%d*%d=%d\t", n, i, i*n)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("2、==========")
+	for i := 9; i >= 1; i-- {
+		for n := 9; n >= i; n-- {
+			fmt.Printf("%d*%d=%d\t", n, i, i*n)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("3、==========")
+	for i := 9; i >= 1; i-- {
+		for n := 1; n <= i; n++ {
+			fmt.Printf("%d*%d=%d\t", n, i, i*n)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("4、==========")
+	for i := 1; i <= 9; i++ {
+		for n := 9; n >= i; n-- {
+			fmt.Printf("%d*%d=%d\t", i, n, i*n)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println("5、==========")
+	m, l := 9, 9
+	for m >= 1 {
+		if l >= 1 {
+			fmt.Printf("%d*%d=%d\t", m, l, m*l)
+			l--
+		} else {
+			m--
+			l = m
+			fmt.Println()
 		}
 	}
+
 }
