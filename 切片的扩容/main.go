eab3d997aca14e1206edaa5a77789ce5a70b6ff5
ConstOwn <constown@foxmail.com>
2021-06-07 15:05:27 +0800

    feat: 🎉切片的扩容

--- 切片的扩容/main.go
+++ 切片的扩容/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// append() 为切片添加元素 原来的底层数组放不下的时候，在底层数组换一个更大的新数组
+// append() 为切片追加元素 原来的底层数组放不下的时候，在底层数组换一个更大的新数组
 func main() {
 	s1 := []string{"北京", "上海", "深圳"}
 	fmt.Printf("s1=%v len(s1)=%d cap(s1)=%d\n", s1, len(s1), cap(s1))
@@ -12,4 +12,13 @@ func main() {
 	fmt.Printf("s1=%v len(s1)=%d cap(s1)=%d\n", s1, len(s1), cap(s1))
 	ss := []string{"杭州", "苏州"}
 	s1 = append(s1, ss...) // ...可以看做JS的扩展运算符
+
+	//	关于 append 删除切片中的某个元素
+	a1 := [...]int{1, 3, 5, 7, 9, 11, 13, 15, 17}
+	a2 := a1[:]
+
+	//	删除索引为 1 的那个 3
+	a2 = append(a2[:1], a2[2:]...)
+	fmt.Println("a2:", a2)
+	fmt.Println("a1:", a1)
 }
