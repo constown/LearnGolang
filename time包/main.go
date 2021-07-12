3a87a9571f768f048d6dd395a00d03be68cb3da8
ConstOwn <constown@foxmail.com>
2021-07-12 16:38:08 +0800

    feat：🎉time包

--- time包/main.go
+++ time包/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	now := time.Now()
+	now := time.Now() // 本地的时间
 	fmt.Println(now)
 	fmt.Println(now.UnixNano())
 	fmt.Println(now.Second())
@@ -24,4 +24,24 @@ func main() {
 		return
 	}
 	fmt.Println(timeObj)
+	n := 100
+	time.Sleep(time.Duration(n))
+
+	// 明天的这个时间
+	// 按照指定格式去解析一个字符串格式的时间
+	time.Parse("2006-01-02 15:04:05", "2021-07-06 23:18:50")
+	// 按照东八区的时区和格式去解析一个字符串格式的时间
+	loc, err := time.LoadLocation("Asia/shanghai")
+	if err != nil {
+		fmt.Printf("load loc faild, err:%v\n", err)
+	}
+	// 按照指定时区解析时间
+	timeObj, err = time.ParseInLocation("2006-01-02 15:04:05", "2021-07-06 23:18:50", loc)
+	if err != nil {
+		fmt.Printf("parse time failed, err:%v\n", err)
+		return
+	}
+	fmt.Println(timeObj, "tiemObj")
+	td := timeObj.Sub(now)
+	fmt.Println(td)
 }
