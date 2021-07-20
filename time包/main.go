ed071dffd7eb80d84b16a339b38b9fb0abd7bffb
ConstOwn <constown@foxmail.com>
2021-07-20 17:09:06 +0800

    doc：🎉笔记补充

--- time包/main.go
+++ time包/main.go
@@ -5,6 +5,34 @@ import (
 	"time"
 )
 
+// 时间格式化 2006-01-02 15：04：05.000
+
+// 时间类型 time.Time
+// 时间戳 time.now().Unix() 1970.1.1到现在的秒数
+// 		 time.now().UnixNano() 纳秒  1970.1.1到现在的纳秒数
+
+// 时间间隔类型 time.Duration  表示两个时间经过的秒数
+//			  time.Second
+//			  time.Hour   等等
+//	const (
+//		Nanosecond  Duration = 1
+//		Microsecond          = 1000 * Nanosecond
+//		Millisecond          = 1000 * Microsecond
+//		Second               = 1000 * Millisecond
+//		Minute               = 60 * Second
+//		Hour                 = 60 * Minute
+//	)
+
+// 时间操作
+// 时间对象 +/- 一个时间间隔对象
+
+//  定时器
+//  time.Tick(time.Second)
+
+// 解析字符串格式时间（时区）按照某一个格式去解析，传过来的字符串
+// time.Parse("2006-01-02 15:04:05", "2021-07-06 23:18:50") 解析成一个utc时间
+// 如果要解析成时区的时间，需要定义一个时区，拿到时区对象后 time.ParseInLocation 解析成时区时间
+
 func main() {
 	now := time.Now() // 本地的时间
 	fmt.Println(now)
@@ -41,7 +69,8 @@ func main() {
 		fmt.Printf("parse time failed, err:%v\n", err)
 		return
 	}
-	fmt.Println(timeObj, "tiemObj")
+	fmt.Println(timeObj, "timeObj")
+	// sub 两个时间相减
 	td := timeObj.Sub(now)
 	fmt.Println(td)
 }
